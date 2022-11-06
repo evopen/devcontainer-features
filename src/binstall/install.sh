eefde6b21925c9fa04246889362b20f1dfdfa5b6
Li Yuanheng <520dhh@gmail.com>
2022-11-06 09:24:21 +0800

    default to latest binstall

--- src/binstall/install.sh
+++ src/binstall/install.sh
@@ -7,6 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get install -y curl
 
 cd /tmp
+echo Downloading cargo-binstall ${VERSION}
 curl -sL https://github.com/cargo-bins/cargo-binstall/releases/download/v${VERSION}/cargo-binstall-x86_64-unknown-linux-gnu.tgz -o binstall.tgz
 tar zxvf binstall.tgz
 mv cargo-binstall /usr/local/bin/
