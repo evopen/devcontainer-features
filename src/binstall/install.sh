15405a69b813ee5f8b62fd1771077881c756fa5f
Li Yuanheng <520dhh@gmail.com>
2022-11-06 09:15:15 +0800

    update

--- src/binstall/install.sh
+++ src/binstall/install.sh
@@ -2,3 +2,10 @@
 set -e
 
 echo "Activating feature 'binstall'"
+
+cd /tmp
+
+curl -sL https://github.com/cargo-bins/cargo-binstall/releases/download/v${VERSION}/cargo-binstall-x86_64-unknown-linux-gnu.tgz -o binstall.tgz
+tar zxvf binstall.tgz
+mv cargo-binstall /usr/local/bin/
+rm binstall.tgz
