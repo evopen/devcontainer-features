33f66647d469d82ac4b65744738e4c68ba73bdaa
Li Yuanheng <520dhh@gmail.com>
2022-11-06 10:03:47 +0800

    binstall (#1)
    * test
    
    * add test
    
    * update
    
    * update
    
    * default to latest binstall
    
    * fixed version to v0.16.0
    
    * fix
    
    * what is this?
    
    * array option test
    
    * install pkg
    
    * delimiter
    
    * again
    
    * use bash
    
    * update
    
    * binstall no confirm
    
    * install to /usr/local/bin
    
    * default to only install cargo-watch

---
+++ src/binstall/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "Activating feature 'binstall'"
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y curl
+
+cd /tmp
+echo Downloading cargo-binstall ${VERSION}
+curl -sL https://github.com/cargo-bins/cargo-binstall/releases/download/v${VERSION}/cargo-binstall-x86_64-unknown-linux-gnu.tgz -o binstall.tgz
+tar zxvf binstall.tgz
+mv cargo-binstall /usr/local/bin/
+rm binstall.tgz
+
+echo Installing $INSTALL
+
+IFS=',' read -ra PKGS <<< "$INSTALL"
+for PKG in "${PKGS[@]}"; do
+  echo Installing $PKG
+  cargo-binstall --install-path /usr/local/bin/ --no-confirm $PKG
+done
